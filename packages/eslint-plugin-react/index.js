a169fa8a17702c376defa09c95fa12ef4ab55168
Nicolas Bazille <nicolas.bazille@m6.fr>
2018-09-26 14:23:56 +0200

    fixup! chore: bump dependencies

--- packages/eslint-plugin-react/index.js
+++ packages/eslint-plugin-react/index.js
@@ -14,16 +14,10 @@ module.exports = {
         ],
 
         // allow ".js" to be a valid extension for JSX files
-        'react/jsx-filename-extension': [
-          'error',
-          { extensions: ['.js', '.jsx'] },
-        ],
+        'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
 
         // TODO: remove these rules when next version of Airbnb config will be released
-        'react/prefer-stateless-function': [
-          'error',
-          { ignorePureComponents: true },
-        ],
+        'react/prefer-stateless-function': ['error', { ignorePureComponents: true }],
         'react/no-unused-prop-types': ['error', { skipShapeProps: true }],
       },
     },
