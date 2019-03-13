8ebc6356a1754c9571460c516a7ae6223a3ecb82
Mathieu Mure <mathieumure@users.noreply.github.com>
2019-03-13 10:51:36 +0100

    chore(eslint-plugin-react): prefer short syntax for fragment (#8)

--- packages/eslint-plugin-react/index.js
+++ packages/eslint-plugin-react/index.js
@@ -15,6 +15,8 @@ module.exports = {
 
         // allow ".js" to be a valid extension for JSX files
         'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+        // prefer "<></>" syntax over <React.Fragment></React.Fragment>
+        'react/jsx-fragments': ['error', 'syntax'],
 
         // TODO: remove these rules when next version of Airbnb config will be released
         'react/prefer-stateless-function': ['error', { ignorePureComponents: true }],
