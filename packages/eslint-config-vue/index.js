a11434e4fcba83a93b542b7b1cece09f23930740
Nicolas Bazille <oltodo@msn.com>
2021-10-22 16:55:56 +0200

    feat: migrate eslint-plugin-vue to eslint-config-vue (#34)

---
+++ packages/eslint-config-vue/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['@bedrockstreaming', 'plugin:vue/recommended'],
+  plugins: ['vue'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  rules: {
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 10,
+      },
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        multiline: 'always',
+      },
+    ],
+    'vue/html-closing-bracket-spacing': [
+      'error',
+      {
+        selfClosingTag: 'always',
+      },
+    ],
+  },
+};
