2aa0bcd7f4665e18cd2cb273ea696d5343783897
Nicolas Bazille <oltodo@msn.com>
2021-12-22 13:25:34 +0100

    fixup! chore: move to @bedrockstreaming organization (#30)

--- packages/eslint-config-vue/typescript.js
+++ packages/eslint-config-vue/typescript.js
@@ -1,10 +1,3 @@
 module.exports = {
   extends: ['@bedrockstreaming/eslint-config/typescript'],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
 };
